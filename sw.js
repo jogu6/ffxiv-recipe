1db8b78290a2309f5c466457ee3cefb175aab20c
rndl_0303 <jun1.ogu6@gmail.com>
2026-06-14 13:34:27 +0900

    v1.15

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // ===== キャッシュバージョン =====
 // データを更新したときはここの番号を上げてください
-const CACHE_VERSION = 'ff14recipe-v1.1';
+const CACHE_VERSION = 'ff14recipe-v1.15';
 
 // ===== キャッシュ対象のリソース種別 =====
 const CACHE_FIRST_PATTERNS = [
